2a1742d5d1a8994e51ef75fa19a4cff29c34d82c
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-16 19:09:33 +0530

    setup shatcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="font-bold p-4 text-3xl">Vite project</div>;
+  return (
+    <div className="p-4">
+      <p className="font-bold p-4 text-3xl">Vite project</p>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
 export default App;

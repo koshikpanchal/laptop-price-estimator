09c4fbdc157bbaa7718c9d2fe568aa0baa241d10
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-09-16 19:14:04 +0530

    setup prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,13 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  return (
-    <div className="p-4">
-      <p className="font-bold p-4 text-3xl">Vite project</p>
-      <Button>Click Me</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <p className="font-bold p-4 text-3xl">Vite project</p>
+         <Button>Click Me</Button>
+      </div>
+   );
 }
 
 export default App;
